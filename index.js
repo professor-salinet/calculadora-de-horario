05302f152decf401178e1a48f835dbb8327a6f6c
Projects repositories of Professor Salinet (SENAC) <rodrigo.silva@docente.pr.senac.br>
2025-07-07 18:04:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ function calcular() {
     const op = document.querySelector('input[name="operacao"]:checked').value;
 
     const fator = parseInt(document.getElementById('fator').value) || 0;
-    const alertaErro = document.getElementById('alertaErro');
+    const alertaHorarioInicial = document.getElementById('alertaHorarioInicial');
+    const alertaHorarioFinal = document.getElementById('alertaHorarioFinal');
+    const alertaFator = document.getElementById('alertaFator');
+
+    const somar = document.getElementById('somar');
+    const subtrair = document.getElementById('subtrair');
+    const dividir = document.getElementById('dividir');
+    const multiplicar = document.getElementById('multiplicar');
 
     let total1 = h1 * 3600 + m1 * 60 + s1;
     let total2 = h2 * 3600 + m2 * 60 + s2;
@@ -18,21 +25,55 @@ function calcular() {
 
     switch (op) {
         case 'somar':
-            if (total1 === 0 || total2 === 0) {
-                alertaErro.style.display = 'block';
-                alertaErro.textContent = 'É necessário digitar os dois horários para realizar o cálculo. Por favor, preencha pelo menos um dos campos de horário.';
+            limparAlertas();
+            if (total1 === 0) {
+                alertaHorarioInicial.textContent = 'É necessário digitar um horário inicial para realizar o cálculo. Por favor, preencha pelo menos um dos campos de horário.';
+                alertaHorarioInicial.style.display = 'block';
                 document.getElementById('hora1').focus();
+                return;
+            }
+            if (total2 === 0) {
+                alertaHorarioFinal.textContent = 'É necessário digitar um horário a ser resolvido para realizar o cálculo. Por favor, preencha pelo menos um dos campos de horário.';
+                alertaHorarioFinal.style.display = 'block';
+                document.getElementById('hora2').focus();
                 return;
             }
             resultado = total1 + total2;
             break;
         case 'subtrair':
+            limparAlertas();
+            if (total1 === 0) {
+                alertaHorarioInicial.textContent = 'É necessário digitar um horário inicial para realizar o cálculo. Por favor, preencha pelo menos um dos campos de horário.';
+                alertaHorarioInicial.style.display = 'block';
+                document.getElementById('hora1').focus();
+                return;
+            }
+            if (total2 === 0) {
+                alertaHorarioFinal.textContent = 'É necessário digitar um horário a ser resolvido para realizar o cálculo. Por favor, preencha pelo menos um dos campos de horário.';
+                alertaHorarioFinal.style.display = 'block';
+                document.getElementById('hora2').focus();
+                return;
+            }
             resultado = total1 - total2;
             break;
         case 'multiplicar':
+            limparAlertas();
+            if (fator === 0) {
+                alertaFator.textContent = 'É necessário digitar um fator para realizar o cálculo. Por favor, preencha pelo menos um dos campos de horário.';
+                alertaFator.style.display = 'block';
+                document.getElementById('hora2').focus();
+                return;
+            }
             resultado = total1 * fator;
             break;
         case 'dividir':
+            limparAlertas();
+            if (fator === 0) {
+                alertaFator.textContent = 'É necessário digitar um fator para realizar o cálculo. Por favor, preencha pelo menos um dos campos de horário.';
+                alertaFator.style.display = 'block';
+                document.getElementById('hora2').focus();
+                return;
+            }
             resultado = fator !== 0 ? Math.floor(total1 / fator) : 0;
             break;
     }
@@ -51,12 +92,17 @@ function calcular() {
 function limpar() {
     ['hora1', 'minuto1', 'segundo1', 'fator', 'hora2', 'minuto2', 'segundo2', 'horaResultado', 'minutoResultado', 'segundoResultado'].forEach(id => {
         document.getElementById(id).value = '';
-        document.getElementById('alertaErro').style.display = 'none';
-        document.getElementById('hora1').focus();
     });
+    document.getElementById('hora1').focus();
+    limparAlertas();
     // document.getElementById('somar').checked = true;
 }
 
+function limparAlertas() {
+    document.getElementById('alertaHorarioInicial').style.display = 'none';
+    document.getElementById('alertaHorarioFinal').style.display = 'none';
+    document.getElementById('alertaFator').style.display = 'none';
+}
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('somar').addEventListener('click', function () {
         document.getElementById('resultadoDiv').style.display = 'block';
